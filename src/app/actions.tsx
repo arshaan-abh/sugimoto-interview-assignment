4cd1ff625ac378d01a9560e606b1f809aee69936
arshaan-abh <arshaan.abh@gmail.com>
2024-10-31 14:33:42 +0330

    Added revalidation

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -2,11 +2,14 @@
 
 import { supabase } from "@/lib/supabase/client";
 import { Database } from "../../database.types";
+import { revalidatePath } from "next/cache";
 
 export async function addReview(
   reviewData: Database["public"]["Tables"]["Reviews"]["Insert"]
 ) {
   const { error } = await supabase.from("Reviews").insert([reviewData]);
+
+  revalidatePath("/");
 
   return error;
 }
